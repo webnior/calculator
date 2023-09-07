0c05910304b50165aa924d0803ea7449d0aee520
SonuKumar81800 <sonu.patna0808@gmail.com>
2023-09-08 00:34:38 +0530

    feat(theme): next-theme added for dark and light theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import Image from "next/image";
+"use client"
+
+import { useState } from "react"
+import { useTheme } from "next-themes"
+
+type Theme = "light" | "dark"
 
 export default function Home() {
+  const [theme, setNewTheme] = useState<Theme>("light")
+  const { setTheme } = useTheme()
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Development will start soon...
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 dark:bg-black dark:text-white">
+      <button>Development will start soon</button>
     </main>
-  );
+  )
 }
