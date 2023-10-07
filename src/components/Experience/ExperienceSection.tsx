58edc26fb88ae9ef33e1cc15a0c8307ca95e80f9
Mukesh Kumar Bagaria <mkbagaria13@gmail.com>
2023-10-08 01:23:21 +0530

    feat(updated exp section): message

--- src/components/Experience/ExperienceSection.tsx
+++ src/components/Experience/ExperienceSection.tsx
@@ -6,9 +6,9 @@ import ExperienceCard from "./ExperienceCard"
 
 function ExperienceSection() {
   return (
-    <div className="">
-      <h2 className="exp-heading">Experience Technovita Solution</h2>
-      <div className="grid grid-cols-6 gap-6">
+    <div className="mx-20 mt-10">
+      <h2 className="text-2xl font-bold">Experience Technovita Solution</h2>
+      <div className="grid lg:grid-cols-6 lg:gap-6 sm:grid-cols-3 sm:gap-3 ml-7 mt-5 justify-items-center">
         <ExperienceCard />
         <ExperienceCard />
         <ExperienceCard />
