08684213377c1ddbc282e3a23382cd74f1a37e77
SonuKumar81800 <sonu.patna0808@gmail.com>
2023-10-30 14:26:22 +0530

    feat(navbar): icons added to each nav menu

--- src/components/navbar/CustomNavBar.tsx
+++ src/components/navbar/CustomNavBar.tsx
@@ -20,6 +20,13 @@ import {
   SquaresPlusIcon,
   XMarkIcon,
 } from "@heroicons/react/24/outline"
+import { FcAbout } from "react-icons/fc"
+import { GrAmazon } from "react-icons/gr"
+import { LiaArtstation } from "react-icons/lia"
+import { MdWork } from "react-icons/md"
+import { RiContactsFill } from "react-icons/ri"
+import { SiCoinmarketcap, SiFlipkart } from "react-icons/si"
+import { TbBrandShopee } from "react-icons/tb"
 
 import { Logo } from "./Logo"
 
@@ -28,31 +35,31 @@ const calculator = [
     name: "Amazon",
     description: "Get a better understanding traffic",
     href: "/calculator/amazon",
-    icon: ChartPieIcon,
+    icon: GrAmazon,
   },
   {
     name: "Flipkart",
     description: "Speak directly to your customers",
     href: "/calculator/flipkart",
-    icon: CursorArrowRaysIcon,
+    icon: SiFlipkart,
   },
   {
     name: "Myntra",
     description: "Your customers’ data will be safe",
     href: "/calculator/myntra",
-    icon: FingerPrintIcon,
+    icon: SiCoinmarketcap,
   },
   {
     name: "Shopsy",
     description: "Connect with third-party tools",
     href: "/calculator/shopsy",
-    icon: SquaresPlusIcon,
+    icon: TbBrandShopee,
   },
   {
     name: "AJIO",
     description: "Build strategic funnels that will convert",
     href: "/calculator/ajio",
-    icon: ArrowPathIcon,
+    icon: LiaArtstation,
   },
 ]
 const products = [
@@ -85,6 +92,27 @@ const products = [
     description: "Build strategic funnels that will convert",
     href: "#",
     icon: ArrowPathIcon,
+  },
+]
+
+const company = [
+  {
+    name: "Contac Us",
+    href: "/contact-us",
+    description: "Contact us for details and some more contents",
+    icon: RiContactsFill,
+  },
+  {
+    name: "About Us",
+    href: "/about-us",
+    description: "See About us and our teams",
+    icon: FcAbout,
+  },
+  {
+    name: "Careers",
+    href: "/career",
+    description: "Career Oportunities",
+    icon: MdWork,
   },
 ]
 const callsToAction = [
@@ -123,7 +151,7 @@ export default function CustomNavBar() {
         <Popover.Group className="hidden lg:flex lg:gap-x-12">
           <Popover className="">
             <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
-              Products
+              Services
               <ChevronDownIcon
                 className="h-5 w-5 flex-none text-gray-400"
                 aria-hidden="true"
@@ -390,8 +418,143 @@ export default function CustomNavBar() {
               </Popover.Panel>
             </Transition>
           </Popover>
+          <Popover className="">
+            <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
+              Company
+              <ChevronDownIcon
+                className="h-5 w-5 flex-none text-gray-400"
+                aria-hidden="true"
+              />
+            </Popover.Button>
 
-          <Link
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <Popover.Panel className="absolute left-0 mr-5 top-full z-10 mt-3 w-2/3 max-w-full overflow-hidden rounded-2xl bg-white shadow-lg ring-1 ring-gray-900/5">
+                <div className="flex flex-row justify-between">
+                  <div className=" flex flex-col">
+                    <div className="pl-16 pt-6">
+                      <Link
+                        href="/calculator"
+                        className="text-2xl font-sans font-semibold"
+                      >
+                        Company
+                      </Link>
+                    </div>
+                    <div className="flex flex-row px-10">
+                      <div className="p-2">
+                        {company.map((item) => (
+                          <div
+                            key={item.name}
+                            className="group relative my-2 flex items-center gap-x-2 rounded-xl p-4 text-sm leading-6 hover:bg-gray-100 border-2 border-gray-900/5 cursor-pointer"
+                          >
+                            <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                              <item.icon
+                                className="h-6 w-6 text-gray-600 group-hover:text-indigo-600"
+                                aria-hidden="true"
+                              />
+                            </div>
+                            <div className="flex-auto">
+                              <Link
+                                href={item.href}
+                                className="block font-semibold text-gray-900"
+                              >
+                                {item.name}
+                                <span className="absolute inset-0" />
+                              </Link>
+                              <p className="mt-1 text-gray-600">
+                                {item.description}
+                              </p>
+                            </div>
+                          </div>
+                        ))}
+                      </div>
+                      {/* <div className="p-2">
+                        {calculator.map((item) => (
+                          <div
+                            key={item.name}
+                            className="group relative my-2 flex items-center gap-x-6 rounded-xl p-4 text-sm leading-6 hover:bg-gray-100 border-2 border-gray-900/5 cursor-pointer"
+                          >
+                            <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                              <item.icon
+                                className="h-6 w-6 text-gray-600 group-hover:text-indigo-600"
+                                aria-hidden="true"
+                              />
+                            </div>
+                            <div className="flex-auto">
+                              <Link
+                                href={item.href}
+                                className="block font-semibold text-gray-900"
+                              >
+                                {item.name}
+                                <span className="absolute inset-0" />
+                              </Link>
+                              <p className="mt-1 text-gray-600">
+                                {item.description}
+                              </p>
+                            </div>
+                          </div>
+                        ))}
+                      </div> */}
+                    </div>
+                  </div>
+                  <div className="p-2 flex flex-col justify-between">
+                    <div className="  cursor-pointer max-w-[400px] max-h-[400px]">
+                      <div className="rounded-xl">
+                        <Image
+                          width={390}
+                          height={180}
+                          src="/nav1.webp"
+                          alt={"whatever"}
+                        />
+                      </div>
+                      <h2 className="py-2 font-bold font-[#555]">
+                        Technovita Store OS
+                      </h2>
+
+                      <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing
+                        elit. Cum totam et dolorum.
+                      </p>
+                    </div>
+                    <div>
+                      <button className=" flex w-auto gap-2 text-white content-center justify-around bg-[#2f31ac] rounded-full p-2 place-content-center px-5">
+                        <h1 className="text-base text-center">try now</h1>
+                        <ArrowLongRightIcon
+                          className="h-6 w-6 text-white-600 group-hover:text-indigo-600"
+                          aria-hidden="true"
+                        />
+                      </button>
+                    </div>
+                    <div></div>
+                  </div>
+                </div>
+                <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
+                  {callsToAction.map((item) => (
+                    <Link
+                      key={item.name}
+                      href={item.href}
+                      className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100"
+                    >
+                      <item.icon
+                        className="h-5 w-5 flex-none text-gray-400"
+                        aria-hidden="true"
+                      />
+                      {item.name}
+                    </Link>
+                  ))}
+                </div>
+              </Popover.Panel>
+            </Transition>
+          </Popover>
+
+          {/* <Link
             href="/contact-us"
             className="text-sm font-semibold leading-6 text-gray-900"
           >
@@ -402,10 +565,11 @@ export default function CustomNavBar() {
             className="text-sm font-semibold leading-6 text-gray-900"
           >
             About Us
-          </Link>
+          </Link> */}
           <Link href="/seller-services">seller-services</Link>
-          <Link href="/career">Career</Link>
+          {/* <Link href="/career">Career</Link> */}
         </Popover.Group>
+        <div className="flex lg:flex-1"></div>
       </nav>
       <Dialog
         as="div"
